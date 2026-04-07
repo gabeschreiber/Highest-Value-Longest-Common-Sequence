4b6432f7abf9a7a7a8d88d9b0df2b3a50c9797c4
Tanner Holland <holland.tanner@ufl.edu>
2026-04-06 21:06:48 -0400

    Backtracking algorithm to determine string sequence

--- src/backtracking.py
+++ src/backtracking.py
@@ -1,2 +1,15 @@
-def backtrack(A, B, alphabet, maxVal):
-    print("not yet written")
+def backtrack(A, B, alphabet, m):
+    i, j = len(A), len(B)
+    result = []
+
+    while i > 0 and j > 0:
+        if A[i-1] == B[j-1] and m[i][j] == m[i][j-1] + alphabet[A[i-1]]:
+            result.append(A[i-1])
+            i -= 1
+            j -= 1
+        elif m[i-1][j] >= m[i][j-1]:
+            i -= 1
+        else:
+            j -= 1
+
+    return ''.join(reversed(result))
